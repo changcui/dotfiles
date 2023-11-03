50efce2e6c05fc9c7fe317223e04589ea4491798
changcui <changcui@tencent.com>
2023-11-03 15:39:16 +0800

    add lsp cmp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,6 +4,10 @@ return {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "folke/neodev.nvim",
+    {
+      "j-hui/fidget.nvim",
+      tag = "legacy",
+    },
   },
   config = function()
     local on_attach = function(_, bufnr)
@@ -15,23 +19,21 @@ return {
 
         vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
       end
-
       nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
       nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
       nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
       nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
       nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-      nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-      nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-      nmap('<leader>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      end, '[W]orkspace [L]ist Folders')
+      -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+      -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+      -- nmap('<leader>wl', function()
+      --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+      -- end, '[W]orkspace [L]ist Folders')
       nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
       nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
       nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-      -- nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
       nmap('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
-      nmap("<space>f", function()
+      nmap('<space>f', function()
         vim.lsp.buf.format { async = true }
       end, "[F]ormat code")
     end
@@ -41,7 +43,9 @@ return {
       lua_ls = {},
     }
     require("neodev").setup()
+    require("fidget").setup()
     require("mason").setup()
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
     require("mason-lspconfig").setup({
       ensure_installed = vim.tbl_keys(servers),
       handlers = {
@@ -49,6 +53,7 @@ return {
           require("lspconfig")[server_name].setup {
             settings = servers[server_name],
             on_attach = on_attach,
+            capabilities = capabilities,
           }
         end,
       }
